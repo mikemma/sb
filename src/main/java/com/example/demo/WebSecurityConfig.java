bd2be2c484bb0a5564cbeb2a8682b40b8f16cd20
Mihai M <mike_mma@yahoo.com>
2023-07-09 07:50:10 +0300

    updated websecurity to have swagger, h2 and /products/ api protected by login

--- src/main/java/com/example/demo/WebSecurityConfig.java
+++ src/main/java/com/example/demo/WebSecurityConfig.java
@@ -1,55 +1,41 @@
 package com.example.demo;
 
-import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.SecurityFilterChain;  
-
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class WebSecurityConfig {
-    
-     @Bean
-    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests()
-        .requestMatchers("/").permitAll()
-        .requestMatchers("/api-docs/**").permitAll()
-        .requestMatchers("/swagger-ui/**").permitAll()
-        .requestMatchers("/products/**").permitAll() //TODO: remove
-        .requestMatchers(toH2Console()).permitAll()
-        .and().csrf().ignoringRequestMatchers(toH2Console())
-        .and().formLogin()
-        .and().httpBasic();
-        
-        // Use them only in development or demo environments. NEVER in production !!!
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/").permitAll()
+                        .requestMatchers("/api-docs/**").permitAll()
+                        .anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .formLogin(Customizer.withDefaults());
+
+        // next 2 lines are to access h2-console
         http.csrf().disable();
-         http.headers().frameOptions().disable();
-        
+        http.headers().frameOptions().disable();
+
         return http.build();
     }
 
     @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        return (web) -> web.ignoring().requestMatchers("/v3/api-docs/**");
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("pwd") //in prod use passwordEncoder()
+                .roles("USER").build();
+        return new InMemoryUserDetailsManager(user);
     }
-
-   @Bean
-	public UserDetailsService userDetailsService() {
-		UserDetails user =
-			 User.withDefaultPasswordEncoder()
-				.username("user")
-				.password("password")
-				.roles("USER")
-				.build();
-
-		return new InMemoryUserDetailsManager(user);
-	} 
 
 }
